539ca40cc2c1080689fecd05f0b22e9870829d18
Justin Patel <pateljustin428@gmail.com>
2021-02-05 16:31:44 +0530

    appointments table created

--- src/routes/Content/Appointments.js
+++ src/routes/Content/Appointments.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AdvancedTableA from './../Tables/ExtendedTable/components/AdvancedTableA';
+import { Container } from './../../components';
+import { HeaderMain } from './../components/HeaderMain';
 
 const Appointments = () => {
     return(
-        <h1>Appointments</h1>
+        <Container>
+            <HeaderMain title="Appointments" className="mb-5 mt-4"/>
+            <AdvancedTableA />
+        </Container>
     );
 }
 
