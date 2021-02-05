8369e713f873618840657b0f67dbc12c99c7d637
Justin Patel <pateljustin428@gmail.com>
2021-02-05 23:35:40 +0530

    added providers table

--- src/routes/Content/Providers.js
+++ src/routes/Content/Providers.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React from "react";
+import ProvidersTable from "./../Tables/ExtendedTable/components/ProvidersTable";
+import { Container } from './../../components';
+import { HeaderMain } from './../components/HeaderMain';
 
 const Providers = () => {
-    return(
-        <h1>Providers</h1>
-    );
-}
+  return (
+    <Container>
+      <HeaderMain title="Providers" className="mb-5 mt-4" />
+      <ProvidersTable />
+    </Container>
+  );
+};
 
 export default Providers;
